9545ca3871b8161e35cb6313c314645ecc359176
diana-nikolaeva <nikol-di@yandex.ru>
2025-02-04 13:53:07 +0300

    router ssg

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,29 @@
 import './assets/main.css'
 
-import { createApp } from 'vue'
 import App from './App.vue'
-import router from './router'
+// import router from './router'
+import { ViteSSG } from 'vite-ssg'
+import HomePageContent from '@/components/homePageContent.vue'
+import SearchPageContent from '@/components/searchPageContent.vue'
 
-const app = createApp(App)
-
-app.use(router)
-
-app.mount('#app')
+export const createApp = ViteSSG(
+  // the root component
+  App,
+  // vue-router options
+  { routes: [
+      {
+        path: '/',
+        name: 'home',
+        component: HomePageContent,
+      },
+      {
+        path: '/search',
+        name: 'search',
+        component: SearchPageContent,
+      }
+    ], },
+  // function to have custom setups
+  ({ app, router, routes, isClient, initialState }) => {
+    // install plugins etc.
+  },
+)
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,29 +1,29 @@
-import HomePageContent from '@/components/homePageContent.vue'
-import SearchPageContent from '@/components/searchPageContent.vue'
-import { createRouter, createWebHistory } from 'vue-router'
+// import HomePageContent from '@/components/homePageContent.vue'
+// import SearchPageContent from '@/components/searchPageContent.vue'
+// import { createRouter, createWebHistory } from 'vue-router'
 
-const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomePageContent,
-    },
-    {
-      path: '/search',
-      name: 'search',
-      component: SearchPageContent,
-    },
-    // {
-    //   path: '/about',
-    //   name: 'about',
-    //   // route level code-splitting
-    //   // this generates a separate chunk (About.[hash].js) for this route
-    //   // which is lazy-loaded when the route is visited.
-    //   component: () => import('../views/AboutView.vue'),
-    // },
-  ],
-})
+// const router = createRouter({
+//   history: createWebHistory(import.meta.env.BASE_URL),
+//   routes: [
+//     {
+//       path: '/',
+//       name: 'home',
+//       component: HomePageContent,
+//     },
+//     {
+//       path: '/search',
+//       name: 'search',
+//       component: SearchPageContent,
+//     },
+//     // {
+//     //   path: '/about',
+//     //   name: 'about',
+//     //   // route level code-splitting
+//     //   // this generates a separate chunk (About.[hash].js) for this route
+//     //   // which is lazy-loaded when the route is visited.
+//     //   component: () => import('../views/AboutView.vue'),
+//     // },
+//   ],
+// })
 
-export default router
+// export default router
